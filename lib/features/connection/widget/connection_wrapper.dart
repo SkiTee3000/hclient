a1ad05fbd517ae170c08f23570b1bb5f2b90e6c2
SkiTee3000 <39069192+SkiTee3000@users.noreply.github.com>
2024-03-19 14:54:01 +0300

    Old (#1)
    * enable auto connect
    
    * 1
    
    * fix
    
    * fix2

--- lib/features/connection/widget/connection_wrapper.dart
+++ lib/features/connection/widget/connection_wrapper.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hiddify/core/localization/translations.dart';
 import 'package:hiddify/core/notification/in_app_notification_controller.dart';
 import 'package:hiddify/features/config_option/notifier/config_option_notifier.dart';
+import 'package:hiddify/core/preferences/service_preferences.dart';
 import 'package:hiddify/features/connection/notifier/connection_notifier.dart';
 import 'package:hiddify/features/profile/notifier/active_profile_notifier.dart';
 import 'package:hiddify/utils/custom_loggers.dart';
+import 'package:hiddify/utils/platform_utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ConnectionWrapper extends StatefulHookConsumerWidget {
@@ -44,15 +46,14 @@ class _ConnectionWrapperState extends ConsumerState<ConnectionWrapper>
   @override
   void initState() {
     super.initState();
-    // remove for now...
-    //
-    // Future.delayed(const Duration(seconds: 2)).then(
-    //   (_) async {
-    //     if (ref.read(startedByUserProvider) && PlatformUtils.isDesktop) {
-    //       loggy.debug("previously started by user, trying to connect");
-    //       return ref.read(connectionNotifierProvider.notifier).mayConnect();
-    //     }
-    //   },
-    // );
+    
+    Future.delayed(const Duration(seconds: 5)).then(
+      (_) async {
+        if (ref.read(startedByUserProvider) && PlatformUtils.isDesktop) {
+          loggy.debug("previously started by user, trying to connect");
+          return ref.read(connectionNotifierProvider.notifier).mayConnect();
+        }
+      },
+    ); 
   }
 }
